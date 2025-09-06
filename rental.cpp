dddffd928cb7b304414279a14cd877dc9efd3b1f
HaiPham215185 <s3975144@rmit.edu.vn>
2025-09-06 09:19:11 +0700

    Add files via upload

---
+++ rental.cpp
@@ -0,0 +1,47 @@
+#include "Rental.h"
+#include <iostream>
+
+using namespace std;
+
+Rental::Rental(member* r, member* o, motorbike* mb, string start, string end)
+{
+    renter = r;
+    owner = o;
+    motorbike = mb;
+    startDate = start;
+    endDate = end;
+    completed = false;
+
+}
+
+void Rental::completedRental()
+{
+    if (!completed){
+        cout << "Rental completed for motorbike " << motorbike->getPlate() << endl;
+        completed = true;
+    } else {
+        cout << "Rental already completed.\n";
+    }
+}
+
+void Rental::rateBoth(int renterStars, string renterComment, int motorbikeStars, string motorbikeComment)
+{
+    if (!completed){
+        cout << "Rental must be completed first.\n";
+        return;
+    }
+
+     // Renter rates motorbike
+    motorbike->addReview(motorbikeStars, renterComment);
+
+    // Owner rates renter
+    renter->addRenterReview(renterStars, motorbikeComment);
+
+    cout << "Both renter and motorbike have been rated.\n";
+}
+
+bool Rental::isCompleted() const {
+    return completed;
+
+
+}
